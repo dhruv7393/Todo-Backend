c2a82d267f4dd774e7ee1848f809f56ed3e27b50
Dhruv Desai <dhruvdesai@Dhruvs-MacBook-Air-2.local>
2024-02-29 14:17:39 -0500

    Adding al outes back

--- src/index.ts
+++ src/index.ts
@@ -22,51 +22,9 @@ app.get("/", (req: express.Request, res: express.Response) => {
 });
 
 app.use("/api/tests", require("./routes/testRoute.js"));
-
-app.listen(port, () => console.log("Server has been initaited"));
-
-
-/*
 app.use("/api/headers", require("./routes/todoHeadersRoute.js"));
 app.use("/api/todos", require("./routes/todoRoute"));
 app.use("/api/dailytask", require("./routes/dailyTaskRoute"));
 app.use("/api/call", require("./routes/callRoute"));
-*/
 
-/*
-
-app.listen(port, () => {
-  console.log(`server is listening on ${port}`);
-});
-
-// Homepage
-app.get('/', (req: express.Request, res: express.Response) => {
-  res.status(200).send("Hello World!");
-});
-
-// GET
-app.get('/get', (req: express.Request, res: express.Response) => {
-  res.status(200).header("x-get-header", "get-header-value").send("get-response-from-compute");
-});
-
-//POST
-app.post('/post', (req: express.Request, res: express.Response) => {
-  res.status(200).header("x-post-header", "post-header-value").send(req.body.toString());
-});
-
-//PUT
-app.put('/put', (req: express.Request, res: express.Response) => {
-  res.status(200).header("x-put-header", "put-header-value").send(req.body.toString());
-});
-
-//PATCH
-app.patch('/patch', (req: express.Request, res: express.Response) => {
-  res.status(200).header("x-patch-header", "patch-header-value").send(req.body.toString());
-});
-
-// Delete
-app.delete('/delete', (req: express.Request, res: express.Response) => {
-  res.status(200).header("x-delete-header", "delete-header-value").send();
-});
-
-*/
+app.listen(port, () => console.log("Server has been initaited"));
