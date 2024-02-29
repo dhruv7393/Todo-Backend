cf5e1a0b9027dbc294de3ba69e41ee019d3e431e
Dhruv Desai <dhruvdesai@Dhruvs-MacBook-Air-2.local>
2024-02-29 13:15:39 -0500

    Updating postbuild

--- bin/postbuild.sh
+++ bin/postbuild.sh
@@ -10,3 +10,8 @@ cp -r ./node_modules ./.amplify-hosting/compute/default/node_modules
 cp -r public ./.amplify-hosting/static
 
 cp deploy-manifest.json ./.amplify-hosting/deploy-manifest.json
+
+cp -r ./src/commonComponents ./.amplify-hosting/compute/default
+cp -r ./src/config ./.amplify-hosting/compute/default
+cp -r ./src/controllers ./.amplify-hosting/compute/default
+cp -r ./src/models ./.amplify-hosting/compute/default
