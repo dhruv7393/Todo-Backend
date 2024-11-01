bdf6ea6e5eacada86d302dd3b246d5801d4f1796
Dhruv Desai <dhruvdesai@Dhruvs-MacBook-Air-2.local>
2024-11-01 16:23:47 -0400

    Adding test task every hour

--- crons.js
+++ crons.js
@@ -1,9 +1,15 @@
 const cron= require('node-cron');
 
 const countAndUpdate = cron.schedule('* * * * *', () => {
-    console.log('running a task every one hour');
-
-    
+  console.log('running a task every hour');
+  fetch("https://main.d3blcxzr0t6m0f.amplifyapp.com/api/tasks", {
+    "headers": {
+      "content-type": "application/json",
+      'Access-Control-Allow-Origin' : '*'
+    },
+    "body": "{\"title\":\"Test\",\"notes\":[\"Test\"],\"imp\":1,\"done\":false,\"pinned\":false}",
+    "method": "POST"
+  });
 });
 
 const startOfCron = () => countAndUpdate.start()
