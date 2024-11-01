2eaf1f4ef165c85ad8afe8f0edbd7f9d6a760719
Dhruv Desai <dhruvdesai@Dhruvs-MacBook-Air-2.local>
2024-11-01 16:49:55 -0400

    Adding test task every hour

--- crons.js
+++ crons.js
@@ -2,14 +2,19 @@ const cron= require('node-cron');
 
 const countAndUpdate = cron.schedule('* * * * *', () => {
   console.log('running a task every hour');
-  fetch("/api/tasks", {
-    "headers": {
-      "content-type": "application/json",
-      'Access-Control-Allow-Origin' : '*'
-    },
-    "body": "{\"title\":\"Test\",\"notes\":[\"Test\"],\"imp\":1,\"done\":false,\"pinned\":false}",
-    "method": "POST"
-  });
+  try {
+    fetch("/api/tasks", {
+      "headers": {
+        "content-type": "application/json",
+        'Access-Control-Allow-Origin' : '*'
+      },
+      "body": "{\"title\":\"Test\",\"notes\":[\"Test\"],\"imp\":1,\"done\":false,\"pinned\":false}",
+      "method": "POST"
+    });
+  }
+  catch(err) {
+    console.log('failed to post');
+  }
 });
 
 const startOfCron = () => countAndUpdate.start()
