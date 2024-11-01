3d6709ce0e49572a2ee2c3063eedf741044b0032
Dhruv Desai <dhruvdesai@Dhruvs-MacBook-Air-2.local>
2024-11-01 14:10:07 -0400

    Adding test task every hour

--- src/routes/dailyTaskRoute.js
+++ src/routes/dailyTaskRoute.js
@@ -3,14 +3,17 @@ const router = express.Router()
 const {getDailyTask, updateDailyTask, setPendingTask} = require('../controllers/dailyTaskController')
 
 router.get('/', (req, res)=>{
+    res.header("Access-Control-Allow-Origin", "*")
     getDailyTask(req, res)
 })
 
-router.patch('/:id', (req, res)=>[
+router.patch('/:id', (req, res)=>{
+    res.header("Access-Control-Allow-Origin", "*")
     updateDailyTask(req, res)
-])
+})
 
 router.get('/updatecount', (req, res)=>{
+    res.header("Access-Control-Allow-Origin", "*")
     setPendingTask(req, res)
 })
 
