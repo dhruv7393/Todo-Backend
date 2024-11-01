5b6c09cbff8d7035ae362aa52ebc9ece7311c8b1
Dhruv Desai <dhruvdesai@Dhruvs-MacBook-Air-2.local>
2024-11-01 17:50:41 -0400

    Removing fetch

--- crons.js
+++ crons.js
@@ -1,20 +1,9 @@
 const cron= require('node-cron');
+import axios from "axios"
 
 const countAndUpdate = cron.schedule('* * * * *', () => {
   console.log('running a task every hour');
-  try {
-    fetch("/api/tasks", {
-      "headers": {
-        "content-type": "application/json",
-        'Access-Control-Allow-Origin' : '*'
-      },
-      "body": "{\"title\":\"Test\",\"notes\":[\"Test\"],\"imp\":1,\"done\":false,\"pinned\":false}",
-      "method": "POST"
-    });
-  }
-  catch(err) {
-    console.log('failed to post');
-  }
+  
 });
 
 const startOfCron = () => countAndUpdate.start()
