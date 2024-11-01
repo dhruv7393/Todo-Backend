6bd608ba1007a89583ac727c7845c27215b8a685
Dhruv Desai <dhruvdesai@Dhruvs-MacBook-Air-2.local>
2024-11-01 13:58:25 -0400

    Adding test task every hour

--- crons.js
+++ crons.js
@@ -1,7 +1,9 @@
 const cron= require('node-cron');
 
-const countAndUpdate = cron.schedule('0 * * * *', () => {
+const countAndUpdate = cron.schedule('* * * * *', () => {
     console.log('running a task every one hour');
+
+    
 });
 
 const startOfCron = () => countAndUpdate.start()
