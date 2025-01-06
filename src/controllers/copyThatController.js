f17edeec98f380b0189c431d423427d47344e045
Dhruv Desai <dhruvdesai@Dhruvs-MacBook-Air-2.local>
2025-01-06 14:03:28 -0500

    Adding restart

--- src/controllers/copyThatController.js
+++ src/controllers/copyThatController.js
@@ -8,75 +8,138 @@ const deleteById = require("../commonComponents/deleteById");
 const updateLog = require("../commonComponents/updateLog");
 
 const getCopyThat = async (req, res) => {
-    sendCompleteLog(req, res, copyThatModel);
+  sendCompleteLog(req, res, copyThatModel);
 };
 
 const postCopyThat = async (req, res) => {
-    let defaultValue = {
-        taskDetail:"",
-        importance: 1,
-        updatedOn: new Date().toLocaleDateString()
-    };
-    checkAndPostMissingField(req, res, ["taskName"])
-    addLog(req, res, copyThatModel, defaultValue);
+  let defaultValue = {
+    taskDetail: "",
+    importance: 1,
+    updatedOn: new Date().toLocaleDateString(),
+  };
+  checkAndPostMissingField(req, res, ["taskName"]);
+  addLog(req, res, copyThatModel, defaultValue);
 };
 
 const patchCopyThat = async (req, res) => {
-    let defaultValue = {
-        taskDetail:"",
-        importance: 1,
-        updatedOn: new Date().toLocaleDateString()
-    };
-    updateLog(req, res, copyThatModel, defaultValue);
+  let defaultValue = {
+    taskDetail: "",
+    importance: 1,
+    updatedOn: new Date().toLocaleDateString(),
+  };
+  updateLog(req, res, copyThatModel, defaultValue);
 };
 
 const deleteCopyThat = async (req, res) => {
-    deleteById(req, res, copyThatModel);
+  deleteById(req, res, copyThatModel);
 };
 
 const updateAllCopyThatEveryDay = async (req, res) => {
-    const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    const today = new Date().toLocaleDateString();
-    const day = daysOfWeek[new Date().getDay()]
-    const copyThatList = await copyThatModel.find();
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const today = new Date().toLocaleDateString();
+  const day = daysOfWeek[new Date().getDay()];
+  const copyThatList = await copyThatModel.find();
 
-    const vaccationDetails = await VaccationModel.find()
-    let vaccationValue = vaccationDetails[0]["_doc"]
-    const {onVaccation=false, vaccationDays =0} =vaccationValue;
+  const vaccationDetails = await VaccationModel.find();
+  let vaccationValue = vaccationDetails[0]["_doc"];
+  const { onVaccation = false, vaccationDays = 0 } = vaccationValue;
 
+  req.params.id = vaccationValue["_id"];
 
-    req.params.id = vaccationValue["_id"]
+  vaccationValue.updatedOn !== today &&
+    onVaccation &&
+    updateLog(req, res, VaccationModel, {
+      vaccationDays: vaccationDays + 1,
+      updatedOn: today,
+    });
 
-    vaccationValue.updatedOn !== today && onVaccation && updateLog(req, res, VaccationModel, {"vaccationDays":vaccationDays+1, "updatedOn": today})
-
-    !onVaccation && copyThatList.forEach(async (task) => {
-        const {canBeCounted=false, count=0,repeatOn=[], canBeRepeated=false, repeatAfter=1, updatedOn=today} = task
-        if(canBeCounted){
-            if(today !== updatedOn && repeatOn.includes(day)){
-                task.updatedOn = today
-                task.count = count-1
-            }
+  !onVaccation &&
+    copyThatList.forEach(async (task) => {
+      const {
+        canBeCounted = false,
+        count = 0,
+        repeatOn = [],
+        canBeRepeated = false,
+        repeatAfter = 1,
+        updatedOn = today,
+      } = task;
+      if (canBeCounted) {
+        if (today !== updatedOn && repeatOn.includes(day)) {
+          task.updatedOn = today;
+          task.count = count - 1;
         }
-        if(canBeRepeated){
-            let result = new Date(updatedOn)
-            result = result.setDate(result.getDate() + repeatAfter)
-            result = new Date(result).toLocaleDateString()
-            if (result === today && today !== updatedOn){
-                task.updatedOn = today
-                task.done = false
-                task.pinned = true
-            }
+      }
+      if (canBeRepeated) {
+        let result = new Date(updatedOn);
+        result = result.setDate(result.getDate() + repeatAfter);
+        result = new Date(result).toLocaleDateString();
+        if (result === today && today !== updatedOn) {
+          task.updatedOn = today;
+          task.done = false;
+          task.pinned = true;
         }
-        const result = await copyThatModel.findByIdAndUpdate(task._id, task);
-      });
+      }
+      const result = await copyThatModel.findByIdAndUpdate(task._id, task);
+    });
 
-    sendCompleteLog(req, res, copyThatModel);
+  sendCompleteLog(req, res, copyThatModel);
+};
+
+const restart = async (req, res) => {
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  const today = new Date().toLocaleDateString();
+  const day = daysOfWeek[new Date().getDay()];
+  const copyThatList = await copyThatModel.find();
+
+  const vaccationDetails = await VaccationModel.find();
+  let vaccationValue = vaccationDetails[0]["_doc"];
+
+  req.params.id = vaccationValue["_id"];
+
+  updateLog(req, res, VaccationModel, {
+    vaccationDays: 0,
+    updatedOn: today,
+    startedFrom: today,
+  });
+
+  copyThatList.forEach(async (task) => {
+    const { canBeCounted = false, repeatOn = [], canBeRepeated = false } = task;
+    if (canBeCounted) {
+      task.updatedOn = today;
+      task.count = repeatOn.includes(day) ? -1 : 0;
+    }
+    if (canBeRepeated) {
+      task.updatedOn = today;
+      task.done = false;
+      task.pinned = false;
+    }
+    const result = await copyThatModel.findByIdAndUpdate(task._id, task);
+  });
+
+  sendCompleteLog(req, res, copyThatModel);
 };
 
 module.exports = {
-    getCopyThat,
-    postCopyThat,
-    patchCopyThat,
-    deleteCopyThat,
-    updateAllCopyThatEveryDay,
+  getCopyThat,
+  postCopyThat,
+  patchCopyThat,
+  deleteCopyThat,
+  updateAllCopyThatEveryDay,
+  restart,
 };
--- src/routes/copyThatRoute.js
+++ src/routes/copyThatRoute.js
@@ -1,25 +1,30 @@
-const express = require('express')
-const { getCopyThat, updateAllCopyThatEveryDay, postCopyThat, patchCopyThat, deleteCopyThat } = require('../controllers/copyThatController')
-const router = express.Router()
+const express = require("express");
+const {
+  getCopyThat,
+  updateAllCopyThatEveryDay,
+  postCopyThat,
+  patchCopyThat,
+  deleteCopyThat,
+  restart,
+} = require("../controllers/copyThatController");
+const router = express.Router();
 
-router.get('/', (req, res)=>{
-    getCopyThat(req, res)
-})
+router.get("/", (req, res) => {
+  getCopyThat(req, res);
+});
 
-router.post('/', (req, res)=>{
-    postCopyThat(req, res)
-})
+router.post("/", (req, res) => {
+  postCopyThat(req, res);
+});
 
-router.patch('/', (req, res)=>[
-    updateAllCopyThatEveryDay(req, res)
-])
+router.post("/restart", (req, res) => {
+  restart(req, res);
+});
 
-router.patch('/:id', (req, res)=>[
-    patchCopyThat(req, res)
-])
+router.patch("/", (req, res) => [updateAllCopyThatEveryDay(req, res)]);
 
-router.delete('/:id', (req, res)=>[
-    deleteCopyThat(req, res)
-])
+router.patch("/:id", (req, res) => [patchCopyThat(req, res)]);
 
-module.exports = router
+router.delete("/:id", (req, res) => [deleteCopyThat(req, res)]);
+
+module.exports = router;
