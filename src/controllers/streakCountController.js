98339fb2a0cf2178aad46f0f69a6bdd6893aae80
Dhruv Desai <dhruvdesai@Dhruvs-MacBook-Air-2.local>
2025-04-17 19:39:15 -0400

    Add api to update sincerity

--- src/controllers/streakCountController.js
+++ src/controllers/streakCountController.js
@@ -8,6 +8,18 @@ const getStreakCount = async (req, res) => {
   sendCompleteLog(req, res, StreakCountModel);
 };
 
+const updateSincerityCount = async (req, res) => {
+  const streakCountDetails = await StreakCountModel.find();
+  let streak = streakCountDetails[0]["_doc"];
+  streak.sincerity[req.params.id] += 1;
+  const resultStreak = await StreakCountModel.findByIdAndUpdate(
+    streak._id,
+    streak
+  );
+  sendCompleteLog(req, res, StreakCountModel);
+};
+
 module.exports = {
   getStreakCount,
+  updateSincerityCount,
 };
--- src/routes/streakCountRoute.js
+++ src/routes/streakCountRoute.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const { getStreakCount } = require("../controllers/streakCountController");
+const {
+  getStreakCount,
+  updateSincerityCount,
+} = require("../controllers/streakCountController");
 
 router.get("/", (req, res) => {
   getStreakCount(req, res);
+});
+
+router.post("/:id", (req, res) => {
+  updateSincerityCount(req, res);
 });
 
 module.exports = router;
