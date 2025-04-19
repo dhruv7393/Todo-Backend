22f78163ae077d7b1c5a2cea10eca315d0e65dbb
Dhruv Desai <dhruvdesai@Dhruvs-MacBook-Air-2.local>
2025-04-19 12:36:29 -0400

    Testing new chron

--- src/controllers/chronContoller.js
+++ src/controllers/chronContoller.js
@@ -43,10 +43,10 @@ const getInitialStreakData = async (req, res) => {
   );
   const sincerelyDone = [];
 
-  if (taskDetailsNitya.repeatOn.includes(prevDay)) {
+  if (taskDetailsNitya.repeatOn.includes(today)) {
     sincerelyDone.push(...taskDetailsNitya.checked);
   }
-  if (taskDetailsWeekly.repeatOn.includes(prevDay)) {
+  if (taskDetailsWeekly.repeatOn.includes(today)) {
     sincerelyDone.push(...taskDetailsWeekly.checked);
   }
 
@@ -69,7 +69,7 @@ const resetTaskForToday = async (req, res) => {
   taskDetails.forEach(async (task) => {
     const { _id, repeatOn, checked, reset } = task;
     const checkForRepeatOn =
-      (typeof repeatOn === "string" && new Date(repeatOn) <= new Date(today)) ||
+      (typeof repeatOn === "string" && repeatOn === today) ||
       repeatOn.includes(dayOfWeek);
 
     if (reset && checkForRepeatOn) {
@@ -87,6 +87,74 @@ const runChron = async (req, res) => {
   sendCompleteLog(req, res, StreakCountModel);
 };
 
+const runChron1 = async (req, res) => {
+  //get details to check if you are on vaccation
+  const streakCountDetails = await StreakCountModel.find();
+  let streak = streakCountDetails[0]["_doc"];
+  let { onVaccation, updatedOn } = streak.onVaccation;
+  if (!onVaccation && updatedOn !== today) {
+    streak.updatedOn = today;
+    streak.activeDays += 1;
+    if (dayOfWeek !== "Sunday" || dayOfWeek !== "Saturday") {
+      streak.weekDays += 1;
+    }
+  }
+
+  // get details of tasks to be carried out daily
+  const taskDetailsNitya = await TasksModel.findById(
+    "67f58c5d0b3ae40bdb5b3438"
+  );
+  const taskDetailsWeekly = await TasksModel.findById(
+    "67f58d060b3ae40bdb5b343e"
+  );
+  const sincerelyDone = [];
+
+  // today is saturady , so add task completed on friday, monday gets leverage
+  if (taskDetailsNitya.repeatOn.includes(today)) {
+    sincerelyDone.push(...taskDetailsNitya.checked);
+  }
+  if (taskDetailsWeekly.repeatOn.includes(today)) {
+    sincerelyDone.push(...taskDetailsWeekly.checked);
+  }
+
+  // add to sincerely done
+  if (updatedOn !== today) {
+    sincerelyDone.forEach((todo) => {
+      streak.sincerity[todo] += 1;
+    });
+  }
+
+  console.log(streak);
+
+  //dhruv
+  /*const resultStreak = await StreakCountModel.findByIdAndUpdate(
+    streak._id,
+    streak
+  );*/
+
+  const taskDetails = await TasksModel.find();
+
+  taskDetails.forEach(async (task) => {
+    const { _id, repeatOn, checked, reset } = task;
+    const checkForRepeatOn =
+      (typeof repeatOn === "string" && repeatOn === today) ||
+      repeatOn.includes(dayOfWeek);
+
+    if (reset && checkForRepeatOn) {
+      task.checked = typeof checked === "number" ? 0 : [];
+      task.repeatOn = typeof repeatOn === "string" ? today : repeatOn;
+
+      //dhruv
+      /*
+      const result = await TasksModel.findByIdAndUpdate(_id, task);*/
+    }
+
+    console.log(task);
+  });
+
+  sendCompleteLog(req, res, StreakCountModel);
+};
+
 module.exports = {
-  runChron,
+  runChron1,
 };
--- src/routes/chron.js
+++ src/routes/chron.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const { runChron } = require("../controllers/chronContoller");
+const { runChron1 } = require("../controllers/chronContoller");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  runChron(req, res);
+  runChron1(req, res);
 });
 
 module.exports = router;
